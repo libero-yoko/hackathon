3d807b42acffad9425c54194b7dca1297220f911
Yoko Ocarina <info@studio-libero.com>
2021-03-10 19:32:35 -0800

    This is the first commit

---
+++ content.js
@@ -0,0 +1,4 @@
+// window.alert('You opend a website!');
+// var myAudio = new Audio(chrome.runtime.getURL("/Volumes/ExternalHDD/Documents/05_Codesmith/Hackathon/meow.mp3"));
+var myAudio = new Audio("meow.mp3");
+myAudio.play();
